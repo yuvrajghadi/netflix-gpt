e646033fab738b0a30d381d5c16f3b8c8493e342
yuvrajghadi <ghadiyuvraj08@gmail.com>
2025-09-25 00:06:37 +0530

    added Login and Header also added SignIN & SignUP form

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <img className="w-52  px-8 py-2 absolute" src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="Logo" />
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Login.js
@@ -0,0 +1,68 @@
+import Header from "./Header";
+import { useState } from "react";
+
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
+  return (
+    <div className="relative h-screen w-full">
+      {/* Background image */}
+      <img
+        className="w-full h-full object-cover"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/c95abc7a-8124-4630-bb7a-3b160bdc6de3/web/IN-en-20250915-TRIFECTA-perspective_d3d87aa7-58ed-4c6b-98dc-231ed05ba675_large.jpg"
+        alt="LoginImage"
+      />
+
+      {/* Dark overlay */}
+      <div className="absolute inset-0 bg-black/60"></div>
+
+      {/* Header stays fixed at top */}
+      <div className="absolute top-0 left-0 w-full z-20">
+        <Header />
+      </div>
+
+      {/* Sign-in form centered */}
+      <form className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col bg-black/70 p-8 rounded-md w-1/3 h-3/4 z-20">
+        <div className="flex">
+          <h1 className="text-4xl font-bold text-white m-2">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </h1>
+        </div>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Enter Full Name"
+            className="p-4 m-2 w-96 bg-black/70 border border-gray-600 rounded-md text-white"
+          />
+        )}
+        <input
+          type="email"
+          placeholder="Email or mobile number"
+          className="p-4 m-2 w-96 bg-black/70 border border-gray-600 rounded-md text-white"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 m-2 w-96 bg-black/70 border border-gray-600 rounded-md text-white"
+        />
+        <button className="bg-red-600 p-2 m-2 rounded-md w-96 text-white font">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <div className="text-gray-400 m-2 text-lg font-semibold text-center">
+          <h2>OR</h2>
+        </div>
+        <p className="text-white  cursor-pointer" onClick={toggleForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up now"
+            : "Already have an account? Sign In"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;

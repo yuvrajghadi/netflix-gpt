64e93aa2d35d00d3e85d208cc0b9b10c39a2a26d
yuvrajghadi <ghadiyuvraj08@gmail.com>
2025-10-09 11:14:12 +0530

    change in tailwind

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,8 +1,10 @@
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="w-screen h-screen pt-72 px-10 absolute bg-gradient-to-r from-black to-transparent">
-      <h1 className="text-5xl font-bold px-4 w-8/12">{title}</h1>
-      <p className="text-lg w-1/2 p-4">
+    <div className="w-screen h-screen pt-56 px-10 absolute bg-gradient-to-r from-black to-transparent">
+    <div className=" h-1/4 top-20 p-7  px-1 w-5/12 absolute">
+      
+      <h1 className="text-5xl font-bold px-4 ">{title}</h1>
+      <p className="text-lg  p-4">
         {overview}
       </p>
       <div className="flex gap-4 px-4">
@@ -12,6 +14,7 @@ const VideoTitle = ({ title, overview }) => {
         <button className="bg-gray-700 opacity-95 p-4 w-40 font-bold text-lg rounded-md hover:opacity-75">
           More Info
         </button>
+      </div>
       </div>
     </div>
   );

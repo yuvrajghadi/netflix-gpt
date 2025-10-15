a1d2bb0e14d947b50f36e2faf599c184a2b26820
yuvrajghadi <ghadiyuvraj08@gmail.com>
2025-10-15 07:39:35 +0530

    chore: temporarily enable dangerous mode in GoogleAI for testing

--- src/utils/googleAi.js
+++ src/utils/googleAi.js
@@ -2,7 +2,8 @@ import { GoogleGenAI } from "@google/genai";
 import { GEMINI_API_KEY } from "./constants";
 
 const ai = new GoogleGenAI({
-    apiKey:GEMINI_API_KEY
+    apiKey:GEMINI_API_KEY,
+    deangourgas: true
 });
 
 export default ai;
